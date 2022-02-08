3ecb1a31702c265e94e7f2637c7c2ab313a794bb
angeloper <angeloper@angeloperui-MacBookPro.local>
2022-02-08 15:23:22 +0900

    complete study websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", (event) => {
   console.log("Connected to Server");
@@ -9,7 +15,10 @@ socket.addEventListener("open", (event) => {
 
 socket.addEventListener("message", (message) => {
   // console.log("Just got this : ", message.data, " from to Server");
-  console.log("New Message : ", message.data);
+  // console.log("New Message : ", message.data);
+  const li = document.createElement("li");
+  li.innerHTML = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -24,8 +33,19 @@ function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
   // console.log(input.value);
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
+  const li = document.createElement("li");
+  li.innerHTML = `You: ${input.value}`;
+  messageList.append(li);
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
   input.value = "";
 }
 
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -21,18 +21,29 @@ const sockets = [];
 // Websocket 관련사항
 wss.on("connection", (socket) => {
   sockets.push(socket); // 접속이 되면 socket 자체를 넣어놓음. (모든 socket 연결된 사람에게 전송하기 위해서)
-
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser"); // frontend에서 websocket으로 연결되었다는 정보를 backend에서 받을 수 있음.
-  socket.send("hello"); // frontend에 hello라는 메세지 전달 (단, frontend 단에서 receiver로 데이터를 받아야 함.)
+  // socket.send("hello"); // frontend에 hello라는 메세지 전달 (단, frontend 단에서 receiver로 데이터를 받아야 함.)
 
   socket.on("close", () => console.log("Disconnected from the Browser")); // Browser가 닫히면 실행됨.
   // frontend가 보낸 메세지를 backend에서 받을수 있도록 함.
-  socket.on("message", (message) => {
+  socket.on("message", (msg) => {
     // console.log("receiver from client message : ", message.toString("utf8"));
     // console.log(message.toString("utf8"));
     // socket.send(message.toString("utf8"));
     // 하기 사항은 소켓으로 연결된 모든 frontend에게 메세지를 보내기 위한 사항임.
-    sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+    // sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
+    const message = JSON.parse(msg.toString("utf8"));
+    switch (message.type) {
+      case "new_message":
+        // sockets.forEach((aSocket) => aSocket.send(message.payload));
+        sockets.forEach((aSocket) =>
+          aSocket.send(`${socket.nickname} : ${message.payload}`)
+        );
+      case "nickname":
+        socket["nickname"] = message.payload;
+      // console.log(message.payload);
+    }
   });
 });
 
