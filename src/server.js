89a0f9555773220ead87cd3cb6772dc5bff730a5
angeloper <angeloper@angeloperui-MacBookPro.local>
2022-02-09 22:45:34 +0900

    install socket.io

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import WebSocket from "ws";
 import express from "express";
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -11,40 +11,12 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// app.listen(3000, handleListen);
 
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-// Websocket 관련사항
-wss.on("connection", (socket) => {
-  sockets.push(socket); // 접속이 되면 socket 자체를 넣어놓음. (모든 socket 연결된 사람에게 전송하기 위해서)
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser"); // frontend에서 websocket으로 연결되었다는 정보를 backend에서 받을 수 있음.
-  // socket.send("hello"); // frontend에 hello라는 메세지 전달 (단, frontend 단에서 receiver로 데이터를 받아야 함.)
-
-  socket.on("close", () => console.log("Disconnected from the Browser")); // Browser가 닫히면 실행됨.
-  // frontend가 보낸 메세지를 backend에서 받을수 있도록 함.
-  socket.on("message", (msg) => {
-    // console.log("receiver from client message : ", message.toString("utf8"));
-    // console.log(message.toString("utf8"));
-    // socket.send(message.toString("utf8"));
-    // 하기 사항은 소켓으로 연결된 모든 frontend에게 메세지를 보내기 위한 사항임.
-    // sockets.forEach((aSocket) => aSocket.send(message.toString("utf8")));
-    const message = JSON.parse(msg.toString("utf8"));
-    switch (message.type) {
-      case "new_message":
-        // sockets.forEach((aSocket) => aSocket.send(message.payload));
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-      // console.log(message.payload);
-    }
-  });
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
