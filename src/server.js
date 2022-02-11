93682bf0e812a0c5b6996b5d975512dbc8520e43
angeloper <angeloper@angeloperui-MacBookPro.local>
2022-02-12 00:47:03 +0900

    socketIO의 Backend에서 Frontend 함수 호출

--- src/server.js
+++ src/server.js
@@ -16,7 +16,11 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  console.log(socket);
+  //console.log(socket);
+  socket.on("enter_room", (msg, done) => {
+    console.log(msg);
+    setTimeout(() => done(), 1000);
+  });
 });
 
 httpServer.listen(3000, handleListen);
