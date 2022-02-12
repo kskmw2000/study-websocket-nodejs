6f658b6cc1b5f483246674538a468f5641a0d62f
angeloper <angeloper@angeloperui-MacBookPro.local>
2022-02-12 20:16:41 +0900

    user video

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 import http from "http";
 import express from "express";
-import { Server } from "socket.io";
-const { instrument } = require("@socket.io/admin-ui");
+import SocketIO from "socket.io";
 
 const app = express();
 
@@ -11,66 +10,8 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
-  cors: {
-    origin: ["*"],
-    credentials: true,
-  },
-});
+const wsServer = SocketIO(httpServer);
 
-instrument(wsServer, {
-  auth: false,
-});
-
-function publicRooms() {
-  // const sids = wsServer.sockets.adapter.sids;
-  // const rooms = wsServer.sockets.adapter.rooms;
-  const { sids, rooms } = wsServer.sockets.adapter;
-
-  const publicRooms = [];
-  rooms.forEach((_, key) => {
-    if (sids.get(key) === undefined) {
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
-function countRoom(roomName) {
-  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", (socket) => {
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    console.log(wsServer.sockets.adapter);
-    console.log(`Socket Event:${event}`);
-  });
-
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName);
-    done(); // frontend의 function 을 호출할 수 있음.
-    socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // socket.io는 나를 제외한 사람에게 메시지를 보냄
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)
-    );
-  });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms());
-  });
-  // 새로운 메세지가 받으면 다음과 같이 실행
-  socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-    done();
-  });
-  // 닉네임
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-});
-
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
